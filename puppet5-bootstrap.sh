6e6e0c8626ea7a5fd68f506a9120342f89839aea
Robert Chipperfield <robert.chipperfield@red-gate.com>
2018-05-02 17:23:00 +0100

    I can't bash.

--- puppet5-bootstrap.sh
+++ puppet5-bootstrap.sh
@@ -40,7 +40,7 @@ elif [ "$dist" == "\"Ubuntu\"" ]; then
 	RELEASE_NAME=`lsb_release -c -s`
 	if [ "$RELEASE_NAME" == "bionic" ]; then
 		RELEASE_NAME=stretch
-	elif
+	fi
     wget https://apt.puppetlabs.com/puppet5-release-${RELEASE_NAME}.deb || exit 1
     dpkg -i puppet5-release-`lsb_release -c -s`.deb || exit 1
     apt-get update || exit 1
