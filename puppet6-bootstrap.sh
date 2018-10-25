91586f9832d4ec1d7c7af4e30a539ee712da7470
Robert Chipperfield <robert.chipperfield@red-gate.com>
2018-10-25 11:49:26 +0100

    Add Puppet 6 bootstrap scripts

---
+++ puppet6-bootstrap.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "Usage: sudo puppet6-bootstrap.sh" 1>&2
+   # exit 1
+fi
+
+# Make sure we have a sensible hostname
+read -p "Enter a hostname for this machine: " NEWHOSTNAME
+hostname $NEWHOSTNAME
+echo $NEWHOSTNAME > /etc/hostname
+
+read -p "Enter puppet master hostname: " PUPPETMASTER
+
+read -p "Enter puppet master port (8140 is the normal one): " MASTERPORT
+
+read -p "Enter Puppet environment name: " PUPPETENV
+
+read -p "Set extra certificate attributes? [y/N]:" SET_EXTRA_ATTRIBUTES
+if [ $SET_EXTRA_ATTRIBUTES ] && [ ${SET_EXTRA_ATTRIBUTES,,} == "y" ]; then
+	read -p "pp_environment: " PP_ENVIRONMENT
+	read -p "pp_service: " PP_SERVICE
+	read -p "pp_role: " PP_ROLE
+fi
+
+# Download and install puppet
+mkdir setup-temp
+cd setup-temp
+
+dist=`awk -F= '/^NAME/{print $2}' /etc/os-release`
+
+if [ "$dist" == "\"CentOS Linux\"" ]; then
+    version=`awk -F= '/^VERSION_ID/{print $2}' /etc/os-release`
+    wget https://yum.puppetlabs.com/puppet6/puppet6-release-el-${version//\"}.noarch.rpm || exit 1
+    rpm -Uvh puppet5-release-el-${version//\"}.noarch.rpm || exit 1
+    yum update || exit 1
+    yum install puppet-agent -y || exit 1
+elif [ "$dist" == "\"Ubuntu\"" ]; then
+	RELEASE_NAME=`lsb_release -c -s`
+	if [ "$RELEASE_NAME" == "bionic" ]; then
+		RELEASE_NAME=stretch
+	fi
+    wget https://apt.puppetlabs.com/puppet6-release-${RELEASE_NAME}.deb || exit 1
+    dpkg -i puppet6-release-${RELEASE_NAME}.deb || exit 1
+    apt-get update || exit 1
+    apt-get install puppet-agent || exit 1
+else
+    echo "Not Ubuntu or CentOS. Aborting."
+    exit 1
+fi
+
+# Add puppet to this session's PATH (the installer will sort it for future sessions)
+export PATH=$PATH:/opt/puppetlabs/bin
+
+# Find the server we're using
+
+/opt/puppetlabs/bin/puppet config set server $PUPPETMASTER --section main
+
+
+/opt/puppetlabs/bin/puppet config set masterport $MASTERPORT --section main
+
+# Set the environment
+
+/opt/puppetlabs/bin/puppet config --section agent set environment $PUPPETENV
+
+# If we're setting extra cert attributes, do that now
+if [ $SET_EXTRA_ATTRIBUTES ] && [ ${SET_EXTRA_ATTRIBUTES,,} == "y" ]; then
+	echo "extension_requests:" >> /etc/puppetlabs/puppet/csr_attributes.yaml
+	[ $PP_ENVIRONMENT ] && echo "    pp_environment: $PP_ENVIRONMENT" >> /etc/puppetlabs/puppet/csr_attributes.yaml
+	[ $PP_SERVICE ] && echo "    pp_service: $PP_SERVICE" >> /etc/puppetlabs/puppet/csr_attributes.yaml
+	[ $PP_ROLE ] && echo "    pp_role: $PP_ROLE" >> /etc/puppetlabs/puppet/csr_attributes.yaml
+fi
+
+# Initial puppet run!
+/opt/puppetlabs/bin/puppet agent -t
+
+echo "Sign and classify the node on the puppet master, then press enter"
+read dummy
+
+# Enable puppet
+/opt/puppetlabs/bin/puppet agent --enable
+
+# First real puppet run
+/opt/puppetlabs/bin/puppet agent -t || exit 1
---
+++ puppet6-rpi-bootstrap.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "Usage: sudo puppet6-rpi-bootstrap.sh" 1>&2
+   exit 1
+fi
+
+# Make sure we have a sensible hostname
+echo "Enter a hostname for this machine: "
+read NEWHOSTNAME
+hostname $NEWHOSTNAME
+echo $NEWHOSTNAME > /etc/hostname
+
+echo "Enter the puppetmaster hostname"
+read MASTER
+
+echo "Enter puppet master port (8140 is the normal one): "
+read MASTERPORT
+
+echo "Enter the environment name"
+read PUPPETENV
+
+echo "Thanks. Beginning set up. This compiles from source so will likely take a long time..."
+# add puppet repo
+mkdir setup-temp
+cd setup-temp
+
+apt-get update || exit 1
+apt-get install ruby-full facter hiera unzip -y || exit 1
+gem install bundler
+
+wget https://github.com/puppetlabs/puppet/archive/6.0.3.tar.gz || exit 1
+tar xzf 6.0.3.tar.gz || exit 1
+cd puppet-6.0.3
+
+bundle install --path .bundle/gems || exit 1
+bundle update || exit 1
+ruby install.rb || exit 1
+
+# Note: we used to add /opt/puppetlabs/bin to the path here, but it would seem Puppet 5.5 at least puts it in /usr/bin/puppet anyway, so no need...
+
+puppet config set server $MASTER --section main || exit 1
+
+puppet config set masterport $MASTERPORT --section main
+
+puppet config set environment $PUPPETENV --section agent || exit 1
+
+puppet agent -t
+
+echo 'Sign this node on the master and press [enter] here when done...'
+read dummy
+
+# Enable puppet
+puppet agent --enable
+
+# Run it for reals
+puppet agent -t
+
+# Enable the service
+puppet resource service puppet ensure=running enable=true
