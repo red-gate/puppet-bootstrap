b05eab65751d4fe4dc070ebc56fd0b4cb5526d5a
Robert Chipperfield <robert.chipperfield@red-gate.com>
2019-01-30 18:34:48 +0000

    Update Raspberry Pi agent to 6.2.0

--- puppet6-rpi-bootstrap.sh
+++ puppet6-rpi-bootstrap.sh
@@ -31,9 +31,9 @@ apt-get install ruby-full facter hiera unzip -y || exit 1
 gem install bundler
 gem install semantic_puppet
 
-wget https://github.com/puppetlabs/puppet/archive/6.0.4.tar.gz || exit 1
-tar xzf 6.0.4.tar.gz || exit 1
-cd puppet-6.0.4
+wget https://github.com/puppetlabs/puppet/archive/6.2.0.tar.gz || exit 1
+tar xzf 6.2.0.tar.gz || exit 1
+cd puppet-6.2.0
 
 bundle install --path .bundle/gems || exit 1
 bundle update || exit 1
